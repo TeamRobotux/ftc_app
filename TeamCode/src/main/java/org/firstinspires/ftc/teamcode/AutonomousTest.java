fc11dfd733ba9d4a20b906f4aba9df2087e971d2
DESKTOP - Jack Lowry <jackthechanFTW@gmail.com>
2017-09-25 21:17:42 -0400

    Created a test autonomous opmode for finding a certain columm of the cryptobox. Has not been tested

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -0,0 +1,103 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+
+
+@Autonomous(name="Autonomous Red Cryptobox Column Sensor", group="Pushbot")
+public class AutonomousTest extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    TestHardware robot           = new TestHardware();   // Use a Pushbot's hardware
+    // could also use HardwarePushbotMatrix class.
+    // double          clawOffset      = 0;                       // Servo mid position
+    //final double    CLAW_SPEED      = 0.02 ;                   // sets rate to move servo
+
+    @Override
+    public void runOpMode() {
+        int column = 0;
+        int targetColumn = 3; //CHANGE TO REGISTER FROM THE IMAGE; IS 3 FOR TESTING
+
+
+
+
+        boolean grabberState = false;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //Move right, scanning for the colored edges of each column
+            double red = robot.colorSensor.blue();
+            if(red > 2) {
+                column++;
+
+            }
+
+            telemetry.addData("column", column);
+            telemetry.addData("target", targetColumn);
+
+            if(column == targetColumn) {
+                //MOVE SOME WAY TO GET THE BLOCK PLACER IN POSITION
+                //THEN, STOP THE OPMODE, STAYING IN THE SAFEZONE
+                stop();
+
+            }
+            else {
+                //Move the robot right some more
+                robot.driveFrontL.setPower(.5);
+                robot.driveFrontR.setPower(-.5);
+                robot.driveRearL.setPower(.5);
+                robot.driveRearR.setPower(-.5);
+                sleep(100);
+            }
+
+
+        }
+    }
+}

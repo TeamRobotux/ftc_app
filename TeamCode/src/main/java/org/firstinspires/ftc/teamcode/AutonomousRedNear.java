772c870e2d55ac1731b8c81d5c8ec227aa9bd6d3
Laptop - Jack Lowry <jackthechanFTW@gmail.com>
2017-11-10 15:14:55 -0500

    Created functional autonomous code for starting positions near relic area.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedNear.java
@@ -0,0 +1,101 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * A test to check the capabilites of encoders
+ */
+
+@Autonomous(name="AutoRedNear", group="Pushbot")
+public class AutonomousRedNear extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    RobotHardware robot = new RobotHardware();   // Use a Pushbot's hardware
+
+    @Override
+    public void runOpMode() {
+
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        robot.grabber.close();
+        waitForStart();
+
+
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("heading:", robot.gyro.imu.getAngularOrientation().firstAngle);
+        telemetry.update();
+
+        while(opModeIsActive()) {
+
+            // Wait for the game to start (driver presses PLAY)
+            //Inches == 37.25
+            robot.wheels.driveDistance(28.4);
+            while (robot.wheels.isBusy()) {
+                sleep(100);
+            }
+            float xAngle = robot.gyro.imu.getAngularOrientation().firstAngle;
+
+            while (xAngle < -92 || xAngle > -88) {
+                if (xAngle < -90) {
+                    robot.wheels.turn(.5);
+                } else {
+                    robot.wheels.turn(-.25);
+                }
+                xAngle = robot.gyro.imu.getAngularOrientation().firstAngle;
+
+                telemetry.addData("heading: ", xAngle);
+                telemetry.update();
+            }
+
+            robot.wheels.turn(0);
+
+            robot.wheels.driveDistance(3.75);
+            while (robot.wheels.isBusy()) {
+                sleep(100);
+            }
+            robot.grabber.open();
+            sleep(500);
+            stop();
+
+        }
+    }
+}
+
+

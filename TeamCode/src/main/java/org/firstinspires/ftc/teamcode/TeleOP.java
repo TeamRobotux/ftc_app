1a4484cabe3d2403f4e2278a319760a6c5e0b233
Jack Lowry <jackthechanFTW@gmail.com>
2018-08-19 17:41:32 -0400

    Fixed Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -49,7 +49,6 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Pushbot: Teleop POV", group="Pushbot")
-@Disabled
 public class TeleOP extends LinearOpMode {
 
     /* Declare OpMode members. */
